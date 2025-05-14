05285daffc166e452535658e356fc33ac96da808
madelinemberger <maddiemt8@gmail.com>
2025-05-13 17:14:41 -1000

    added kauai and changed injection wells so they are on their own

--- workflow/setup.R
+++ workflow/setup.R
@@ -12,3 +12,11 @@ data_dir <- here("data")
 out_dir <- here("outputs")
 
 
+dropbox_dir <- c(
+  'Windows' = paste0('C:/Users/', Sys.getenv("USERNAME"), "/Donovan Lab Dropbox/Donovan Lab Team Folder/Donovan_Lab_GIS"),
+  'Linux' = "~/Donovan Lab Dropbox/Donovan Lab Team Folder/Donovan_Lab_GIS/",
+  'Darwin' = "~/Donovan Lab Dropbox/Donovan Lab Team Folder/Donovan_Lab_GIS/"
+)[[Sys.info()[['sysname']]]]
+
+
+driver_dir <- file.path(dropbox_dir,"Drivers")
