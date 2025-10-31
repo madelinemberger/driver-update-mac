8351c7c5648999858884e5ec7e0e5de47bc06092
madelinemberger <maddiemt8@gmail.com>
2025-10-31 13:31:46 -0700

    save progress on Oahu permits clean.

--- workflow/setup.R
+++ workflow/setup.R
@@ -14,8 +14,8 @@ out_dir <- here("outputs")
 
 dropbox_dir <- c(
   'Windows' = paste0('C:/Users/', Sys.getenv("USERNAME"), "/Donovan Lab Dropbox/Donovan Lab Team Folder/Donovan_Lab_GIS"),
-  'Linux' = "~/Donovan Lab Dropbox/Donovan Lab Team Folder/Donovan_Lab_GIS/",
-  'Darwin' = "~/Donovan Lab Dropbox/Donovan Lab Team Folder/Donovan_Lab_GIS/"
+  'Linux' = "~/Donovan Lab Dropbox/Donovan Lab Team Folder/Donovan_Lab_GIS",
+  'Darwin' = "~/Donovan Lab Dropbox/Donovan Lab Team Folder/Donovan_Lab_GIS"
 )[[Sys.info()[['sysname']]]]
 
 
