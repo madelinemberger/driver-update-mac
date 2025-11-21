a1deca5b68af807898ec9ccc810814a08a9bde2f
madelinemberger <maddiemt8@gmail.com>
2025-11-20 15:23:17 -1000

    google api not working much better

--- OSDS-script/osm_api_nondwells.R
+++ OSDS-script/osm_api_nondwells.R
@@ -32,6 +32,47 @@ iws_nodwell_address <- iws_permits_withnodwell %>%
 
 unique_full_address <- unique(iws_nodwell_address$full_address)
 
+# ------------ Clean up addresses with custom function ------------------ #
+
+clean_oahu_address <- function(x) {
+  
+  # convert to character
+  x <- as.character(x)
+  
+  # trim whitespace
+  x <- str_trim(x)
+  
+  # Remove multiple spaces
+  x <- str_replace_all(x,"\\s+", " ")
+  
+  # replace 'OAHU' (any case) with 'HI'
+  x <- str_replace_all(x, "(?i)\\bOahu\\b", "HI")
+  
+  # expand common Hawaii abbreviations, spec for Kam Highway
+  x <- str_replace_all(x, "(?i)\\bKAM HWY\\b|\\bKAME HWY\\b", "Kamehameha Hwy")
+  x <- str_replace_all(x, "(?i)\\bHWY\\b", "Highway")
+  
+  # add comma before HI
+  x <- str_replace_all(x, " HI\\b", ", HI")
+  
+  # add comma between city and HI if missing
+  # example: "Kapolei HI" → "Kapolei, HI"
+  x <- str_replace(x, "([A-Za-z])\\s*,?\\s*HI", "\\1, HI")
+  
+  # clean up double commas
+  x <- str_replace_all(x, ",+", ",")
+  
+  # normalize comma spacing: " , " → ", "
+  x <- str_replace_all(x, "\\s*,\\s*", ", ")
+  
+  # fix stray spaces again if any
+  x <- str_replace_all(x, "\\s+", " ")
+  
+  return(x)
+}
+
+clean_addresses <- lapply(unique_full_address, clean_oahu_address)
+
 # --------- Function to return info from OSM --------------- #
 
 get_place_type_osm <- function(address) {
@@ -58,13 +99,13 @@ get_place_type_osm <- function(address) {
 }
 
 # Test with an address for a park:
-get_place_type_osm("3840 Paki Avenue, Honolulu, HI")
+get_place_type_osm("67-224 Kupahu Street Waialua 96791 Oahu")
 
 # Try with a list
 
 results <- lapply(unique_full_address, get_place_type_osm)
 
-results <- unlist(results)
+#results <- unlist(results) # ok most of these are NA
 
 
 # ---------------------- Function to reverse Geocode from Lat and Lons -------------------------- #
@@ -82,9 +123,71 @@ get_reverse_osm <- function(lat, lon) {
       addressdetails = 1,
       polygon_geojson = 1
     ),
-    user_agent("MyAddressLookup/1.0 (your_email@example.com)")
+    user_agent("GetPlaces (mmtb@hawaii.edu)")
   )
   
   txt <- content(res, as = "text", encoding = "UTF-8")
   fromJSON(txt)
 }
+
+# ------------------- Try using Google Places -------------------------------- # 
+
+google_key <- "AIzaSyC0bWnCXknnXgz-HtroSVdEs1xhp88GROg"
+
+get_place_type_google <- function(address){
+  url <- "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+  
+  res <- GET(url, query = list(
+    input = address,
+    inputtype = "textquery",
+    fields = "name,types,formatted_address",
+    key = google_key
+  ))
+  
+  txt <- content(res, as = "text", encoding = "UTF-8")
+  data <- fromJSON(txt)
+  
+  if (length(data$candidates) == 0) {
+    return(list(name = NA, address = NA, type = NA))
+  }
+  
+  out <- data$candidates
+  
+  # Handles both data.frame and list cases
+  if (is.data.frame(out)) {
+    name  <- out$name[1]
+    addr  <- out$formatted_address[1]
+    types <- paste(out$types[[1]], collapse = ", ")
+  } else if (is.list(out)) {
+    name  <- out[[1]]$name
+    addr  <- out[[1]]$formatted_address
+    types <- paste(out[[1]]$types, collapse = ", ")
+  } else {
+    return(list(name = NA, address = NA, type = NA))
+  }
+  
+  return(list(name = name, address = addr, type = types))
+  
+  }
+
+
+get_place_type_google("3840 Paki Avenue, Honolulu, HI")
+
+
+# test it on our address list 
+
+clean_address_10 <- head(clean_addresses, 30)
+
+
+results_30 <- lapply(clean_address_10,get_place_type_google)
+
+df_30 <- data.frame(
+  input_address = unlist(clean_address_10),
+  name  = sapply(results_10, function(x) x$name),
+  full_address = sapply(results_10, function(x) x$address),
+  type  = sapply(results_10, function(x) x$type),
+  stringsAsFactors = FALSE
+)
+
+df_30
+
