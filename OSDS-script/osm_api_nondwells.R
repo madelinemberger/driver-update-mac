a7276936d4dcf22b731400b015d797556316e303
madelinemberger <maddiemt8@gmail.com>
2026-03-26 13:19:06 -1000

    removed public key

--- OSDS-script/osm_api_nondwells.R
+++ OSDS-script/osm_api_nondwells.R
@@ -75,7 +75,6 @@ clean_addresses <- lapply(unique_full_address, clean_oahu_address)
 
 # --------- Function to return info from OSM --------------- #
 
-#AIzaSyBeVFcbESzDgIwQGw5mER1bdsoLC9mXc4M <- key
 
 #iggy says save each call to a file and then you dont have to request the API too much
 
